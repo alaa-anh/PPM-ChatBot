48bffb556d15aa9cf645fe00dd2717e963e5e677
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-04-25 10:33:22 +0300

    24/4 err

--- Common/ProjectServerTeamAPI.cs
+++ Common/ProjectServerTeamAPI.cs
@@ -1212,151 +1212,151 @@ namespace Common
                 else if (completionpercentVal == 90)
                     jToken = jArrays.Where(t => (int?)t["ProjectPercentCompleted"] < 100);
 
-                if (ProjectSEdateFlag == "START")
-                {
-                    if (FilterType.ToUpper() == "BEFORE" && pStartDate != "")
-                        jToken = jArrays.Where(t => (DateTime?)t["ProjectStartDate"] <= DateTime.Parse(pStartDate));
+                //if (ProjectSEdateFlag == "START")
+                //{
+                //    if (FilterType.ToUpper() == "BEFORE" && pStartDate != "")
+                //        jToken = jArrays.Where(t => (DateTime?)t["ProjectStartDate"] <= DateTime.Parse(pStartDate));
 
-                    else if (FilterType.ToUpper() == "AFTER" && pStartDate != "")
-                        jToken = jArrays.Where(t => (DateTime?)t["ProjectStartDate"] >= DateTime.Parse(pStartDate));
+                //    else if (FilterType.ToUpper() == "AFTER" && pStartDate != "")
+                //        jToken = jArrays.Where(t => (DateTime?)t["ProjectStartDate"] >= DateTime.Parse(pStartDate));
 
-                    else if (FilterType.ToUpper() == "BETWEEN" && pStartDate != "")
-                        jToken = jArrays.Where(t => (DateTime?)t["ProjectStartDate"] >= DateTime.Parse(pStartDate) && (DateTime?)t["ProjectStartDate"] <= DateTime.Parse(PEndDate));
-                }
-                else if (ProjectSEdateFlag == "Finish")
-                {
-                    if (FilterType.ToUpper() == "BEFORE" && PEndDate != "")
-                        jToken = jArrays.Where(t => (DateTime?)t["ProjectFinishDate"] <= DateTime.Parse(PEndDate));
+                //    else if (FilterType.ToUpper() == "BETWEEN" && pStartDate != "")
+                //        jToken = jArrays.Where(t => (DateTime?)t["ProjectStartDate"] >= DateTime.Parse(pStartDate) && (DateTime?)t["ProjectStartDate"] <= DateTime.Parse(PEndDate));
+                //}
+                //else if (ProjectSEdateFlag == "Finish")
+                //{
+                //    if (FilterType.ToUpper() == "BEFORE" && PEndDate != "")
+                //        jToken = jArrays.Where(t => (DateTime?)t["ProjectFinishDate"] <= DateTime.Parse(PEndDate));
 
-                    else if (FilterType.ToUpper() == "AFTER" && PEndDate != "")
-                        jToken = jArrays.Where(t => (DateTime?)t["ProjectFinishDate"] >= DateTime.Parse(PEndDate));
-                    else if (FilterType.ToUpper() == "BETWEEN" && PEndDate != "")
-                        jToken = jArrays.Where(t => (DateTime?)t["ProjectFinishDate"] >= DateTime.Parse(pStartDate) && (DateTime?)t["ProjectFinishDate"] <= DateTime.Parse(PEndDate));
-                }
+                //    else if (FilterType.ToUpper() == "AFTER" && PEndDate != "")
+                //        jToken = jArrays.Where(t => (DateTime?)t["ProjectFinishDate"] >= DateTime.Parse(PEndDate));
+                //    else if (FilterType.ToUpper() == "BETWEEN" && PEndDate != "")
+                //        jToken = jArrays.Where(t => (DateTime?)t["ProjectFinishDate"] >= DateTime.Parse(pStartDate) && (DateTime?)t["ProjectFinishDate"] <= DateTime.Parse(PEndDate));
+                //}
 
-                if (jToken.Any())
-                {
-                    if (jToken.Count() > 0)
-                    {
-                        int inDexToVal = SIndex + 10;
-                        Counter = jToken.Count();
-                        if (inDexToVal >= jToken.Count())
-                            inDexToVal = jToken.Count();
+                //if (jToken.Any())
+                //{
+                //    if (jToken.Count() > 0)
+                //    {
+                //        int inDexToVal = SIndex + 10;
+                //        Counter = jToken.Count();
+                //        if (inDexToVal >= jToken.Count())
+                //            inDexToVal = jToken.Count();
 
-                        //for (int startIndex = SIndex; startIndex < inDexToVal; startIndex++)
-                        //{
-                        //    var item = jToken.ElementAt(startIndex);
-                        //    string SubtitleVal = "";
+                //        for (int startIndex = SIndex; startIndex < inDexToVal; startIndex++)
+                //        {
+                //            var item = jToken.ElementAt(startIndex);
+                //            string SubtitleVal = "";
 
 
-                        //    if (item["ProjectName"] != null)
-                        //        ProjectName = (string)item["ProjectName"];
+                //            if (item["ProjectName"] != null)
+                //                ProjectName = (string)item["ProjectName"];
 
-                        //    if (item["ProjectWorkspaceInternalUrl"] != null)
-                        //        ProjectWorkspaceInternalUrl = (string)item["ProjectWorkspaceInternalUrl"];
+                //            if (item["ProjectWorkspaceInternalUrl"] != null)
+                //                ProjectWorkspaceInternalUrl = (string)item["ProjectWorkspaceInternalUrl"];
 
-                        //    if (item["ProjectPercentCompleted"] != null)
-                        //        ProjectPercentCompleted = (string)item["ProjectPercentCompleted"];
+                //            if (item["ProjectPercentCompleted"] != null)
+                //                ProjectPercentCompleted = (string)item["ProjectPercentCompleted"];
 
-                        //    if (item["ProjectFinishDate"] != null)
-                        //        ProjectFinishDate = (DateTime)item["ProjectFinishDate"];
+                //            if (item["ProjectFinishDate"] != null)
+                //                ProjectFinishDate = (DateTime)item["ProjectFinishDate"];
 
-                        //    if (item["ProjectStartDate"] != null)
-                        //        ProjectStartDate = (DateTime)item["ProjectStartDate"];
+                //            if (item["ProjectStartDate"] != null)
+                //                ProjectStartDate = (DateTime)item["ProjectStartDate"];
 
-                        //    if (item["ProjectDuration"] != null)
-                        //        ProjectDuration = (string)item["ProjectDuration"];
+                //            if (item["ProjectDuration"] != null)
+                //                ProjectDuration = (string)item["ProjectDuration"];
 
-                        //    if (item["ProjectOwnerName"] != null)
-                        //        ProjectOwnerName = (string)item["ProjectOwnerName"];
+                //            if (item["ProjectOwnerName"] != null)
+                //                ProjectOwnerName = (string)item["ProjectOwnerName"];
 
-                        //    SubtitleVal += "Completed Percentage :\n" + ProjectPercentCompleted + "%</br>";
-                        //    SubtitleVal += "Start Date :\n" + ProjectStartDate + "</br>";
-                        //    SubtitleVal += "Finish Date :\n" + ProjectFinishDate + "</br>";
-                        //    SubtitleVal += "Project Duration :\n" + ProjectDuration + "</br>";
-                        //    SubtitleVal += "Project Manager :\n" + ProjectOwnerName + "</br>";
+                //            SubtitleVal += "Completed Percentage :\n" + ProjectPercentCompleted + "%</br>";
+                //            SubtitleVal += "Start Date :\n" + ProjectStartDate + "</br>";
+                //            SubtitleVal += "Finish Date :\n" + ProjectFinishDate + "</br>";
+                //            SubtitleVal += "Project Duration :\n" + ProjectDuration + "</br>";
+                //            SubtitleVal += "Project Manager :\n" + ProjectOwnerName + "</br>";
 
-                        //    string ImageURL = "http://02-code.com/images/logo.jpg";
-                        //    List<CardImage> cardImages = new List<CardImage>();
-                        //    List<CardAction> cardactions = new List<CardAction>();
-                        //    cardImages.Add(new CardImage(url: ImageURL));
-                        //    CardAction btnWebsite = new CardAction()
-                        //    {
-                        //        Type = ActionTypes.OpenUrl,
-                        //        Title = "Open",
-                        //        Value = ProjectWorkspaceInternalUrl + "?redirect_uri={" + ProjectWorkspaceInternalUrl + "}",
-                        //    };
-                        //    CardAction btnTasks = new CardAction()
-                        //    {
-                        //        Type = ActionTypes.PostBack,
-                        //        Title = "Tasks",
-                        //        Value = "show a list of " + ProjectName + " tasks",
-                        //        //  DisplayText = "show a list of " + ProjectName + " tasks",
-                        //        Text = "show a list of " + ProjectName + " tasks",
-                        //    };
-                        //    cardactions.Add(btnTasks);
+                //            string ImageURL = "http://02-code.com/images/logo.jpg";
+                //            List<CardImage> cardImages = new List<CardImage>();
+                //            List<CardAction> cardactions = new List<CardAction>();
+                //            cardImages.Add(new CardImage(url: ImageURL));
+                //            CardAction btnWebsite = new CardAction()
+                //            {
+                //                Type = ActionTypes.OpenUrl,
+                //                Title = "Open",
+                //                Value = ProjectWorkspaceInternalUrl + "?redirect_uri={" + ProjectWorkspaceInternalUrl + "}",
+                //            };
+                //            CardAction btnTasks = new CardAction()
+                //            {
+                //                Type = ActionTypes.PostBack,
+                //                Title = "Tasks",
+                //                Value = "show a list of " + ProjectName + " tasks",
+                //                //  DisplayText = "show a list of " + ProjectName + " tasks",
+                //                Text = "show a list of " + ProjectName + " tasks",
+                //            };
+                //            cardactions.Add(btnTasks);
 
-                        //    CardAction btnIssues = new CardAction()
-                        //    {
-                        //        Type = ActionTypes.PostBack,
-                        //        Title = "Issues",
-                        //        Value = "show a list of " + ProjectName + " issues",
-                        //        Text = "show a list of " + ProjectName + " issues"
-                        //    };
-                        //    cardactions.Add(btnIssues);
+                //            CardAction btnIssues = new CardAction()
+                //            {
+                //                Type = ActionTypes.PostBack,
+                //                Title = "Issues",
+                //                Value = "show a list of " + ProjectName + " issues",
+                //                Text = "show a list of " + ProjectName + " issues"
+                //            };
+                //            cardactions.Add(btnIssues);
 
-                        //    CardAction btnRisks = new CardAction()
-                        //    {
-                        //        Type = ActionTypes.PostBack,
-                        //        Title = "Risks",
-                        //        Value = "Show risks and the assigned resources of " + ProjectName,
-                        //        Text = "Show risks and the assigned resources of " + ProjectName,
+                //            CardAction btnRisks = new CardAction()
+                //            {
+                //                Type = ActionTypes.PostBack,
+                //                Title = "Risks",
+                //                Value = "Show risks and the assigned resources of " + ProjectName,
+                //                Text = "Show risks and the assigned resources of " + ProjectName,
 
-                        //    };
-                        //    cardactions.Add(btnRisks);
+                //            };
+                //            cardactions.Add(btnRisks);
 
-                        //    CardAction btnDeliverables = new CardAction()
-                        //    {
-                        //        Type = ActionTypes.PostBack,
-                        //        Title = "Deliverables",
-                        //        Value = "Show " + ProjectName + " deliverables",
-                        //        Text = "Show " + ProjectName + " deliverables",
-                        //    };
-                        //    cardactions.Add(btnDeliverables);
+                //            CardAction btnDeliverables = new CardAction()
+                //            {
+                //                Type = ActionTypes.PostBack,
+                //                Title = "Deliverables",
+                //                Value = "Show " + ProjectName + " deliverables",
+                //                Text = "Show " + ProjectName + " deliverables",
+                //            };
+                //            cardactions.Add(btnDeliverables);
 
-                        //    CardAction btnAssignments = new CardAction()
-                        //    {
-                        //        Type = ActionTypes.PostBack,
-                        //        Title = "Assignments",
-                        //        Value = "get " + ProjectName + " assignments",
-                        //        Text = "get " + ProjectName + " assignments",
+                //            CardAction btnAssignments = new CardAction()
+                //            {
+                //                Type = ActionTypes.PostBack,
+                //                Title = "Assignments",
+                //                Value = "get " + ProjectName + " assignments",
+                //                Text = "get " + ProjectName + " assignments",
 
-                        //    };
-                        //    cardactions.Add(btnAssignments);
+                //            };
+                //            cardactions.Add(btnAssignments);
 
-                        //    CardAction btnMilestones = new CardAction()
-                        //    {
-                        //        Type = ActionTypes.PostBack,
-                        //        Title = "Milestones",
-                        //        Value = "get " + ProjectName + " milestones",
-                        //        Text = "get " + ProjectName + " milestones",
+                //            CardAction btnMilestones = new CardAction()
+                //            {
+                //                Type = ActionTypes.PostBack,
+                //                Title = "Milestones",
+                //                Value = "get " + ProjectName + " milestones",
+                //                Text = "get " + ProjectName + " milestones",
 
-                        //    };
-                        //    cardactions.Add(btnMilestones);
+                //            };
+                //            cardactions.Add(btnMilestones);
 
-                        //    HeroCard plCard = new HeroCard()
-                        //    {
-                        //        Title = ProjectName,
-                        //        Subtitle = SubtitleVal,
-                        //        Images = cardImages,
-                        //        Buttons = cardactions,
-                        //        Tap = btnTasks,
-                        //    };
-                        //    reply.Attachments.Add(plCard.ToAttachment());
-                        //}
+                //            HeroCard plCard = new HeroCard()
+                //            {
+                //                Title = ProjectName,
+                //                Subtitle = SubtitleVal,
+                //                Images = cardImages,
+                //                Buttons = cardactions,
+                //                Tap = btnTasks,
+                //            };
+                //            reply.Attachments.Add(plCard.ToAttachment());
+                //        }
                        
-                    }
-                }
+                //    }
+                //}
 
                
 
@@ -1364,7 +1364,7 @@ namespace Common
 
             HeroCard plCard2 = new HeroCard()
             {
-                Title = jToken.Count().ToString(),
+                Title = "hii",
 
             };
             reply.Attachments.Add(plCard2.ToAttachment());
